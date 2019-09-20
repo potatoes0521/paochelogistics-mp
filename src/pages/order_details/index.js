a521008da307f6b94dc22a8b635271553539d411
liuyang <liuy@paoche56.com>
2019-09-20 17:57:09 +0800

    小改动

--- src/pages/order_details/index.js
+++ src/pages/order_details/index.js
@@ -2,14 +2,14 @@
  * @Author: guorui
  * @description: 订单详情
  * @Date: 2019-09-20 10:16:14
- * @LastEditors: guorui
- * @LastEditTime: 2019-09-20 17:05:38
+ * @LastEditors: liuYang
+ * @LastEditTime: 2019-09-20 17:53:18
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import OrderDetailsComponent from '@c/order_details/index.js'
 import './index.styl'
 
@@ -31,14 +31,14 @@ export default class OrderDetails extends Component {
         </View>
         <View className='page-footer'>
           <View className='upper-button'>
-            <Button className='pay-button buttons'>立即支付
+            <View className='pay-button buttons'>立即支付
               <Text className='reduce-price'>(立减50元)</Text>
-            </Button>
-            <Button className='share-button buttons'>分享砍价</Button>
+            </View>
+            <View className='share-button buttons'>分享砍价</View>
           </View>
           <View className='lower-button'>
-            <Button className='cancel-button buttons'>取消订单</Button>
-            <Button className='collect-button buttons'>车到付款</Button>
+            <View className='cancel-button buttons'>取消订单</View>
+            <View className='collect-button buttons'>车到付款</View>
           </View>
         </View>
       </View>
