8a98e9a080136b202b948ee1cba829746310ed40
liuyang <liuy@paoche56.com>
2019-09-23 14:40:02 +0800

    把公共没有标题的card组件提取出来

--- src/components/all_order_pages/order_details/index.js
+++ src/components/all_order_pages/order_details/index.js
@@ -2,8 +2,8 @@
  * @Author: guorui
  * @description: 订单详情
  * @Date: 2019-09-20 09:58:08
- * @LastEditors: guorui
- * @LastEditTime: 2019-09-23 11:10:25
+ * @LastEditors: liuYang
+ * @LastEditTime: 2019-09-23 14:31:41
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -12,7 +12,8 @@ import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import './index.styl'
 import OrderComponent from '../order_card/index.js'
-import NoTitleCard from '../no_title_card/index.js'
+// eslint-disable-next-line import/first
+import NoTitleCard from '@c/no_title_card/index.js'
 import ServiceDetailsComponent from '../service_details/index.js'
 import PriceDetailsComponent from '../price_details/index.js'
 
