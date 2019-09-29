2ca8e3a54e0c94ba36c892d895e54a6b7b6e39e8
liuyang <liuy@paoche56.com>
2019-09-29 14:37:46 +0800

    客户信息和个人信息页面

--- src/pages/customer_details/index.js
+++ src/pages/customer_details/index.js
@@ -1,9 +1,9 @@
 /*
  * @Author: liuYang
- * @description: 请填写描述信息
+ * @description: 客户信息详情
  * @Date: 2019-09-27 15:43:53
  * @LastEditors: liuYang
- * @LastEditTime: 2019-09-27 15:45:02
+ * @LastEditTime: 2019-09-29 14:34:24
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -14,13 +14,47 @@ import { connect } from '@tarojs/redux'
 import './index.styl'
 
 class MineInfo extends Component { 
+  navigatorTo() { 
+    Taro.navigateTo({
+      url: '/pages/customer_edit/index'
+    })
+  }
   config = {
     navigationBarTitleText: '客户信息'
   }
   render() { 
     return (
       <View className='page-wrapper'>
-        客户信息 
+        <View className='customer-info-wrapper'>
+          <View className='info-item'>
+            <View className='item-label'>姓名</View>
+            <View className='item-text'>名字</View>
+          </View>
+          <View className='info-item'>
+            <View className='item-label'>联系方式</View>
+            <View className='item-text'>12345678</View>
+          </View>
+          <View className='info-item'>
+            <View className='item-label'>所属经销商</View>
+            <View className='item-text'>上海瑞博祥云汽车服务祥云汽车服务</View>
+          </View>
+          <View className='info-item'>
+            <View className='item-label'>身份证号</View>
+            <View className='item-text'>赚大钱</View>
+          </View>
+          <View className='info-item'>
+            <View className='item-label'>添加时间</View>
+            <View className='item-text'>365789015789781678</View>
+          </View>
+          <View className='info-item'>
+            <View className='item-label'>最后更新时间</View>
+            <View className='item-text'>2019-09-20 15:31:56</View>
+          </View>
+        </View> 
+        <View
+          className='edit-btn'
+          onClick={this.navigatorTo}
+        >修改</View>
       </View>
     )
   }
