4d568f0484baa3922b757042fb0df33637e48998
liuyang <liuy@paoche56.com>
2019-10-08 12:58:37 +0800

    首页修改

--- src/components/checkbox_group/index.js
+++ src/components/checkbox_group/index.js
@@ -3,7 +3,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-09-27 11:02:36
  * @LastEditors: liuYang
- * @LastEditTime: 2019-09-27 13:01:19
+ * @LastEditTime: 2019-10-08 12:19:10
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -11,6 +11,8 @@ import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import '../../assets/icon_font/icon.scss'
+
 import './index.styl'
 
 export default class CheckBoxGroup extends Component {
@@ -30,20 +32,50 @@ export default class CheckBoxGroup extends Component {
    * @return void
    */
   initData() {
-    
+    const { options } = this.props
+    options.forEach((item) => {
+      item.checked = false
+    })
+    this.setState({
+      checkboxList: options
+    })
+  }
+  /**
+   * 点击事件
+   * @param {Object} option 选中项的信息
+   * @return void
+   */
+  handleClick(option) {
+    if (option.disabled) return
+    let {
+      checkboxList
+    } = this.state
+    checkboxList.forEach(ele => {
+      if (ele.id === option.id) {
+        ele.checked = !ele.checked
+      }
+    })
+    this.setState({
+      checkboxList
+    })
+    this.props.onClick(checkboxList, ...arguments)
   }
   render() {
     const {
       checkboxList
     } = this.state
-    const { options } = this.props
-    const checkboxGroupList = options.map(item => {
+
+    const checkboxGroupList = checkboxList.map(item => {
+      const checkboxClassName = classNames('checkbox', {
+        'checked iconfont iconduigoux': item.checked
+      })
       return(
         <View
           key={item}
           className='checkbox-item'
+          onClick={this.handleClick.bind(this, item)}
         >
-          <View className='checkbox'></View>
+          <View className={checkboxClassName}></View>
           <View className='checkbox-label'>{item.label}</View>
         </View>
       )
