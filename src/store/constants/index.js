0bc1449873d083bb8e6f86045f54c0cac9ffbe96
liuyang <liuy@paoche56.com>
2019-10-08 18:24:43 +0800

    删除没用的redux

--- src/store/constants/index.js
+++ src/store/constants/index.js
@@ -3,7 +3,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-09-05 15:02:41
  * @LastEditors: liuYang
- * @LastEditTime: 2019-10-08 18:24:08
+ * @LastEditTime: 2019-10-08 18:24:28
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
