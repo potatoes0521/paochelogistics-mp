67b714b49acb08043aec571fb997b78212daf8a0
guor <guor@paoche56.com>
2019-10-09 09:58:13 +0800

    添加询价单详情逻辑

--- src/api/modules/offer.js
+++ src/api/modules/offer.js
@@ -2,16 +2,36 @@
  * @Author: liuYang
  * @description: 询价单接口
  * @Date: 2019-09-26 11:23:26
- * @LastEditors: liuYang
- * @LastEditTime: 2019-09-27 13:11:05
+ * @LastEditors: guorui
+ * @LastEditTime: 2019-10-08 17:11:10
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import requestHandle from '../request_handle.js';
 
 export default {
+  // 询价单新增
+  addOfferOrder(data, that) {
+    return requestHandle.post(`inquiryorder/add`, data, that);
+  },
+  // 询价单取消询价
+  cancelOffer(data, that) {
+    return requestHandle.post(`inquiryorder/cancel`, data, that);
+  },
+  // 询价单详情
+  getOfferDetails(data, that) {
+    return requestHandle.get(`inquiryorder/detail`, data, that);
+  },
   // 获取询价单列表
   getOfferList(data, that) {
     return requestHandle.get(`inquiryorder/list`, data, that);
+  },
+  // 询价单失效，再次询价
+  getReinquiryOrder(data, that) {
+    return requestHandle.post(`inquiryorder/reinquiry`, data, that);
+  },
+  // 询价单催报价
+  getPromptOffer(data, that) {
+    return requestHandle.post(`inquiryorder/urge`, data, that);
   },
 }
--- src/api/modules/order.js
+++ src/api/modules/order.js
@@ -2,16 +2,28 @@
  * @Author: liuYang
  * @description: 请填写描述信息
  * @Date: 2019-09-26 11:23:34
- * @LastEditors: liuYang
- * @LastEditTime: 2019-09-27 14:56:06
+ * @LastEditors: guorui
+ * @LastEditTime: 2019-10-08 17:23:02
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import requestHandle from '../request_handle.js';
 
 export default {
+  // 获取订单详情
+  getOrderDetails(data, that) {
+    return requestHandle.get(`order/detail`, data, that);
+  },
   // 获取订单列表
   getOrderList(data, that) {
     return requestHandle.get(`order/list`, data, that);
+  },
+  // 订单立即支付
+  payOrder(data, that) {
+    return requestHandle.post(`order/pay`, data, that);
+  },
+  // 订单下单
+  placeOrder(data, that) {
+    return requestHandle.post(`order/place`, data, that);
   },
 }
