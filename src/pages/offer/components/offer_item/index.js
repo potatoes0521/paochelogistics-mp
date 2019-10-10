92bbd4ce9d41271fec08d6db6d9e20c695373232
liuyang <liuy@paoche56.com>
2019-10-10 10:27:51 +0800

    新时间

--- src/pages/offer/components/offer_item/index.js
+++ src/pages/offer/components/offer_item/index.js
@@ -3,7 +3,7 @@
  * @description: 询价单公共组件页面
  * @Date: 2019-09-23 10:49:11
  * @LastEditors: liuYang
- * @LastEditTime: 2019-10-10 09:58:08
+ * @LastEditTime: 2019-10-10 10:27:19
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -50,7 +50,7 @@ export default class OfferItem extends Component {
         onClick={this.navigatorToOfferDetails}
       >
         <View className='item-title-wrapper'>
-          <View className='title-time'>{item.createTime}</View>
+          <View className='title-time'>{item.inquiryTimeDesc}</View>
           <View className={offerStatusClassName}>{item.statusDesc}</View>
         </View>
         <View className='item-main'>
@@ -59,7 +59,7 @@ export default class OfferItem extends Component {
             <Text className='iconfont iconjiantou_qiehuanyou icon-style'></Text>
             <Text>{item.receiveCityName}</Text>
           </View>
-          <View className='list-item'>发车时间：{item.sendTime}</View>
+          <View className='list-item'>发车时间：{item.sendTimeDesc}</View>
           {
             (!item.storePickup && !item.homeDelivery) ?
               null :
