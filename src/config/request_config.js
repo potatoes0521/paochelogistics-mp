16ac78899e4392b92f4b1abcb60c8083da894625
liuyang <liuy@paoche56.com>
2019-10-14 09:47:31 +0800

    修改请求地址

--- src/config/request_config.js
+++ src/config/request_config.js
@@ -3,11 +3,11 @@
  * @description: 服务器状态码错误配置
  * @Date: 2019-08-13 12:22:58
  * @LastEditors: liuYang
- * @LastEditTime: 2019-10-12 18:18:02
+ * @LastEditTime: 2019-10-14 09:25:13
  */
 
 // api地址
-export const defaultApiURL = 'http://192.168.3.113:8085/' // 李斌
+export const defaultApiURL = 'http://192.168.3.123:8085/' // 李斌
 // export const defaultApiURL = 'http://192.168.3.191:8085/' // 测试环境  
 // export const defaultApiURL = 'http://192.168.3.191:8081/' // 测试环境  
 // export const defaultApiURL = 'http://yapi.demo.qunar.com/mock/97800/' // 测试环境
