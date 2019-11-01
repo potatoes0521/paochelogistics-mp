aee9aa31a76104a537da52ace23b67d1f1b7ce4c
liuyang <liuy@paoche56.com>
2019-11-01 16:34:11 +0800

    测试环境用户信息

--- src/store/reducers/user_msg.js
+++ src/store/reducers/user_msg.js
@@ -3,7 +3,7 @@
  * @description: 请填写描述信息
  * @Date: 2019-09-05 15:02:41
  * @LastEditors: liuYang
- * @LastEditTime: 2019-11-01 16:19:38
+ * @LastEditTime: 2019-11-01 16:33:45
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -33,10 +33,10 @@ if (env === 'development') {
     terminalType: 0,
     openId: "11akshahsdgjhasgk", // 测试环境被添加的客户的openid   手机号是  13312345678
     userId: 158,
-    userType: 2, // 13312345678
+    userType: 2,
+    // userType: 0, // 13312345678
     // openId: "oYl965Zw_TG2G_xiNCkS3yRE-FaU", // 测试环境刘洋的openId
     // userId: 144,  // 测试环境刘洋的userId
-    // userType: 1,
     token: "",
     updateTime: "2019-10-18T09:29:44.000+0000",
     userAgent: "",
@@ -47,8 +47,13 @@ export default function counter(state = INITIAL_STATE, action) {
   switch (action.type) {
     case CHANGEUSERINFO:  // 修改用户信息
       if (!action.data) return
-      if (env === 'development' && action.data.token) {
-        const newUserInfoData = Object.assign({}, state.userInfo, action.data)
+      if (env === 'development') {
+        let newUserInfoData = Object.assign({}, state.userInfo)
+        if (action.data.token) {
+          newUserInfoData = Object.assign({}, state.userInfo, {
+            token: action.data.token
+          })
+        }
         return {
           ...state,
           userInfo: newUserInfoData
