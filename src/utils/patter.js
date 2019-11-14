8fcdf811e1b66a98e37aeee5a29ded46c0bb219f
liuyang <liuy@paoche56.com>
2019-11-14 17:06:48 +0800

    添加正则文件

---
+++ src/utils/patter.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: liuYang
+ * @description: 正则文件
+ * @Date: 2019-11-14 16:49:26
+ * @LastEditors: liuYang
+ * @LastEditTime: 2019-11-14 16:54:35
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ */
+
+// 身份证正则
+export const idCardPatter = /^[1-9][0-9]{5}([1][9][0-9]{2}|[2][0][0|1][0-9])([0][1-9]|[1][0|1|2])([0][1-9]|[1|2][0-9]|[3][0|1])[0-9]{3}([0-9]|[X])$/
+// 姓名正则
+export const realNamePatter = /^[\u4E00-\u9FA5]{2,8}$/
+// 手机号正则
+export const phoneNumberPatter = /^1[3456789]\d{9}$/
+// 验证码正则
+export const verificationCodePatter = /^\d{4,6}\b/
