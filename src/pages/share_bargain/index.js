e3eed3e706359925fbcc5d684c976d48ead40f75
liuyang <liuy@paoche56.com>
2020-01-08 10:40:37 +0800

    修复bug

--- src/pages/share_bargain/index.js
+++ src/pages/share_bargain/index.js
@@ -3,7 +3,7 @@
  * @description: 分享砍价
  * @Date: 2019-11-05 13:24:34
  * @LastEditors  : liuYang
- * @LastEditTime : 2020-01-04 13:57:57
+ * @LastEditTime : 2020-01-08 10:40:11
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -235,7 +235,7 @@ class ShareBargain extends Component {
             userInfoFromWX: wxUserInfo
           }, () => {
             requestBargain(this, 0).then(res => {
-              if (res) { 
+              if (res && !res.code) {
                 this.setState({
                   bargainPrice: (res / 100).toFixed(2),
                   showBargainBox: true
@@ -243,7 +243,7 @@ class ShareBargain extends Component {
                 this.getBargainDetails(false)
               } else {
                 Taro.showToast({
-                  title: '您已经砍过价，不能再次砍价',
+                  title: res.message,
                   icon: 'none'
                 })
               }
