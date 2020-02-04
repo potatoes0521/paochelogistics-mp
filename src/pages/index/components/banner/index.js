366c94a637b7e59b29b75db107f284385bfdcc83
liuyang <liuy@paoche56.com>
2020-02-04 13:24:56 +0800

    banner组件化

--- src/pages/index/components/banner/index.js
+++ src/pages/index/components/banner/index.js
@@ -4,7 +4,7 @@
  * @path: 引入路径
  * @Date: 2020-02-03 14:17:04
  * @LastEditors  : liuYang
- * @LastEditTime : 2020-02-03 15:53:33
+ * @LastEditTime : 2020-02-04 13:22:14
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
@@ -22,9 +22,9 @@ import './index.styl'
 
 export default class index extends Component { 
 
-  // static options = {
-    // addGlobalClass: true // 允许外部样式修改组件样式
-  // }
+  static options = {
+    addGlobalClass: true // 允许外部样式修改组件样式
+  }
 
   constructor(props) {
     super(props)
