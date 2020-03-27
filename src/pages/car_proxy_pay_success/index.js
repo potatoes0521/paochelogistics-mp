98a5a03b923221b7eeccd6b1008ab5da29dbf044
liuyang <liuy@paoche56.com>
2020-03-27 10:16:55 +0800

    付款成功

--- src/pages/car_proxy_pay_success/index.js
+++ src/pages/car_proxy_pay_success/index.js
@@ -1,38 +1,82 @@
 /*
  * @Author: liuYang
- * @description: 请填写描述信息
+ * @description: 支付详情
  * @path: 引入路径
  * @Date: 2020-03-17 16:11:16
  * @LastEditors: liuYang
- * @LastEditTime: 2020-03-17 16:11:47
+ * @LastEditTime: 2020-03-27 10:15:31
  * @mustParam: 必传参数
  * @optionalParam: 选传参数
  */
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import classNames from 'classnames'
+import {
+  View,
+  Text
+} from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-import api from '@api/index.js'
+// import api from '@api/index.js'
 
 import './index.styl'
 
-class CarProxy extends Component { 
+class CarProxyPaySuccess extends Component { 
 
   constructor(props) {
     super(props)
-    this.state={}
+    this.state = {
+      payPriceDesc: '',
+      proxyOrderCode: '',
+      createTimeDesc: '',
+      payTimeDesc: ''
+    }
   }
 
   componentDidMount() {
   }
 
   config = {
-    navigationBarTitleText: '我的' 
+    navigationBarTitleText: '支付详情'
   }
 
   render() {
+    let {
+      payPriceDesc,
+      proxyOrderCode,
+      createTimeDesc,
+      payTimeDesc
+    } = this.state
     return (
-      <View></View>
+      <View className='page-wrapper'>
+        <View className='top-bg'>
+          <Text className='iconfont iconzhifuchenggong1 pay-icon'></Text>
+          <Text className='top-tips'>支付成功</Text>
+        </View>
+        <View className='position-wrapper'>
+          <View className='twig'></View>
+          <View className='twig-blur'></View>
+          <View className='order-details-wrapper'>
+            <View className='money-wrapper'>
+              {payPriceDesc && <Text className='money-icon'>¥</Text>}
+              <Text className='money'>{payPriceDesc}</Text>
+            </View>
+            <View className='msg-wrapper'>
+              <View className='public-item'>
+                <View className='public-label'>订单编号</View>
+                <View className='public-content'>{proxyOrderCode || ''}</View>
+              </View>
+              <View className='public-item'>
+                <View className='public-label'>下单时间</View>
+                <View className='public-content'>{createTimeDesc || ''}</View>
+              </View>
+              <View className='public-item'>
+                <View className='public-label'>付款时间</View>
+                <View className='public-content'>{payTimeDesc || ''}</View>
+              </View>
+            </View>
+          </View>
+          <View className='btn-public btn-see'>查看订单</View>
+          <View className='btn-public btn-back'>返回列表页</View>
+        </View>
+      </View>
     )
   }
 
@@ -43,4 +87,4 @@ const mapStateToProps = (state) => {
     userInfo: state.user_msg.userInfo
   }
 }
-export default connect(mapStateToProps)(CarProxy)
+export default connect(mapStateToProps)(CarProxyPaySuccess)
