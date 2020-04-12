37dad3faa0c860c405339828395c105fc2205b35
liuyang <liuy@paoche56.com>
2020-04-12 20:38:39 +0800

    救援icon公共组件

---
+++ src/components/rescue_icon/index.js
@@ -0,0 +1,63 @@
+/*
+ * @Author: liuYang
+ * @description: 请填写描述信息
+ * @path: 引入路径
+ * @Date: 2020-04-12 20:22:09
+ * @LastEditors: liuYang
+ * @LastEditTime: 2020-04-12 20:34:01
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ */
+import Taro, { Component } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+
+import './index.styl'
+
+export default class index extends Component { 
+
+  static options = {
+    addGlobalClass: true // 允许外部样式修改组件样式
+  }
+
+  constructor(props) {
+    super(props)
+    this.state={}
+  }
+
+  componentDidMount() {
+  }
+
+
+  render() {
+    let { type } = this.props
+    const wrapperClassName = classNames('rescue-icon-wrapper', {
+      'big-rescue-icon-wrapper': type === 'big',
+      'small-rescue-icon-wrapper': type === 'small',
+    }) 
+    return (
+      <View
+        className={wrapperClassName}
+      >
+        <View className='iconfont iconjiuyuan rescue-icon'></View>
+        {
+          type === 'big' ?
+            <View className='rescue-icon-text'>救援</View>
+            : null
+        }
+      </View>
+    )
+  }
+
+}
+
+index.defaultProps = {
+  type: 'big',
+  // onClick: () => {}
+}
+
+index.propTypes = {
+  type: PropTypes.string
+  // onClick: PropTypes.func.isRequired
+}
